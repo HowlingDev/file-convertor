532e9e7d8ffb24f0556cb1c2e672f74d765d0750
GrigoriyVolk <grigoriyvolk228@gmail.com>
2026-08-16 17:45:16 +0300

    добавил конвертор для txt

--- file-conversion-service/src/main/java/com/example/converters/TxtToPdfConverter.java
+++ file-conversion-service/src/main/java/com/example/converters/TxtToPdfConverter.java
@@ -1,9 +1,14 @@
 package com.example.converters;
 
+import org.apache.pdfbox.Loader;
+import org.apache.pdfbox.pdmodel.PDDocument;
 import org.springframework.stereotype.Component;
 
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
+
 @Component
-public class TxtToPdfConverter implements Converter {
+public class TxtToPdfConverter extends Converter {
 
     @Override
     public boolean supports(String fileType) {
@@ -11,8 +16,20 @@ public class TxtToPdfConverter implements Converter {
     }
 
     @Override
-    public void convertToPdf() {
+    public void convertToPdf(String fileName) throws Exception {
 
+        super.createBucketIfNotExists();
+
+        String newFileName = super.replaceExtension(fileName, ".pdf");
+
+        try (InputStream inputStream = super.download(fileName)) {
+            byte[] bytes = inputStream.readAllBytes();
+            try (PDDocument doc = Loader.loadPDF(bytes);
+                 ByteArrayOutputStream out = new ByteArrayOutputStream()) {
+                doc.save(out);
+                super.upload(out.toByteArray(), newFileName);
+            }
+        }
     }
 
 }
